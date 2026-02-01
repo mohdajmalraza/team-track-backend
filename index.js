4544ff3ea29b4e1d20ac3b1f932c6cdb848500de
Mohd Ajmal Raza <mohdajmalraza92786@gmail.com>
2026-02-01 23:44:52 +0530

    chore: update cors config to fix the error at production

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ const { authMiddleware } = require("./middlewares/auth.middleware.js");
 const app = express();
 initializeDatabase();
 
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-    // credentials: true,
-  }),
-);
 
 app.post("/auth/signup", signupUser);
 app.post("/auth/login", loginUser);
